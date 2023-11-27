da1621de98447fddd72da63fad47427733919093
Firassaad <firas.saadaoui@esprit.tn>
2023-11-28 00:32:49 +0100

    Committing local changes before merging

--- src/main/java/Frs/api1/Controller/uploadController.java
+++ src/main/java/Frs/api1/Controller/uploadController.java
@@ -1,5 +1,6 @@
 package Frs.api1.Controller;
 
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -13,4 +14,52 @@ public class uploadController {
     public void test(){
       System.out.println("test api access");
     }
+=======
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import Frs.api1.Service.FileUploadService;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:3001")
+public class UploadController {
+
+  // DI with contructor
+  private final FileUploadService fileUploadService;
+
+  public UploadController(FileUploadService fileUploadService) {
+    this.fileUploadService = fileUploadService;
+  }
+
+  @GetMapping("/api")
+  public void test() {
+    System.out.println("test api access");
+  }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
+      @RequestParam("username") String firstName, @RequestParam("password") String lastName) {
+    try {
+      byte[] fileData = file.getBytes();
+      // Vous pouvez utiliser firstName, lastName, et fileData comme nécessaire
+      fileUploadService.saveFile(firstName, lastName, fileData);
+
+      return ResponseEntity.ok("File uploaded successfully");
+    } catch (IOException e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading file");
+    }
+
+  }
+
+>>>>>>> 2ac0210 (commit api consume vue js post request)
 }
